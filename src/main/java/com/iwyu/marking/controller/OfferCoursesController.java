3fa5f116b2b1a0a0a9ab4deb443ba7408c7618c8
Chester <chestermao.9@gmail.com>
2021-04-30 14:38:22 +0800

    作业添加目标点

--- src/main/java/com/iwyu/marking/controller/OfferCoursesController.java
+++ src/main/java/com/iwyu/marking/controller/OfferCoursesController.java
@@ -1,9 +1,14 @@
 package com.iwyu.marking.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.iwyu.marking.dto.OfferCourseDTO;
+import com.iwyu.marking.entity.Course;
+import com.iwyu.marking.entity.CourseObjective;
 import com.iwyu.marking.entity.OfferCourses;
 import com.iwyu.marking.entity.Teacher;
+import com.iwyu.marking.service.CourseObjectiveService;
+import com.iwyu.marking.service.CourseService;
 import com.iwyu.marking.service.OfferCoursesService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -29,8 +35,14 @@ import java.util.List;
 @Api(value = "开设课程前端控制器")
 public class OfferCoursesController {
 
-    @Autowired
+    @Resource
     private OfferCoursesService offerCoursesService;
+
+    @Resource
+    private CourseService courseService;
+
+    @Resource
+    private CourseObjectiveService courseObjectiveService;
 
     @ApiOperation(value = "教师开课")
     @PostMapping(value = "/offerCourse")
@@ -58,6 +70,17 @@ public class OfferCoursesController {
     @GetMapping("/findCourse")
     public OfferCourses findCourse(@RequestParam("offerId") Integer offerId){
         return offerCoursesService.findCourse(offerId);
+    }
+
+    //获取课程目标点
+    @GetMapping("/findObjectives")
+    public List<CourseObjective> findObjectives(@RequestParam("offerId") Integer offerId){
+        OfferCourses offerCourses = offerCoursesService.getById(offerId);
+        Course course = courseService.getById(offerCourses.getCourseId());
+        String[] objective = course.getCourseObjectives().split(",");
+        QueryWrapper<CourseObjective> queryWrapper = new QueryWrapper<>();
+        queryWrapper.in("objective_id",objective);
+        return courseObjectiveService.list(queryWrapper);
     }
 }
 
